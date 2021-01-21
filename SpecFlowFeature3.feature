6410d11e6d72f6f40e7dedad60ec1afb694219fe
Philip Schulz-klingauf <psk@schulz-klingauf.de>
2021-01-21 17:18:32 +0100

    all tests running

---
+++ SpecFlowFeature3.feature
@@ -0,0 +1,10 @@
+﻿Feature: SpecFlowFeature3
+	To perform calculations
+	As a user
+	I want to be told the Tangens of a number
+
+@mytag
+Scenario: Tangens of a number
+	Given [TAN] the first number is 5
+	When [TAN] the Cosinus calculation is performed
+	Then [TAN] the result should be -3.38052
---
+++ SpecFlowFeature3Steps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace wi19b101_rqebb
+{
+    [Binding]
+    public class SpecFlowFeature3Steps
+    {
+        double number;
+        double actual;
+
+        [Given(@"\[TAN] the first number is (.*)")]
+        public void GivenTANTheFirstNumberIs(int p0)
+        {
+            number = p0;
+        }
+        
+        [When(@"\[TAN] the Cosinus calculation is performed")]
+        public void WhenTANTheCosinusCalculationIsPerformed()
+        {
+            actual = Calculator.GetTan(number);
+        }
+        
+        [Then(@"\[TAN] the result should be (.*)")]
+        public void ThenTANTheResultShouldBe(Decimal p0)
+        {
+            var exptected = (double)p0;
+            Assert.Equal(exptected, actual, 5);
+        }
+    }
+}
